26ecb98d1aad97ba14bea12378cd513084b14a53
Mike Chester Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-05-13 15:56:58 +0800

    Create control-plane-ubuntu20.04.sh

---
+++ control-plane-ubuntu20.04.sh
@@ -0,0 +1,2 @@
+curl -s https://raw.githubusercontent.com/mikechesterwang/setup-kubeadm/main/setup-ubuntu20.04.sh | sudo bash
+sudo kubeadm init --cri-socket unix:///var/run/crio/crio.sock --pod-network-cidr 10.244.0.0/16 --control-plane-endpoint $(hostname):6443
