71e2430d9a6a3f33c83e52ae8de52a1f9c324bb1
Mike Chester Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-05-14 15:42:24 +0800

    Update setup-ubuntu20.04.sh

--- setup-ubuntu20.04.sh
+++ setup-ubuntu20.04.sh
@@ -38,9 +38,3 @@ sudo apt-get update
 sudo apt-get install -y kubelet=1.21.0-00 kubeadm=1.21.0-00 kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# setup kubectl
-unset KUBECONFIG
-mkdir -p $HOME/.kube
-sudo rm -f $HOME/.kube/config
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
