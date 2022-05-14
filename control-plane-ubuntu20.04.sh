f8af9d540fe47321c81c11dd7b048e3c4b5b77e3
Mike Chester Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-05-14 14:48:16 +0800

    Update control-plane-ubuntu20.04.sh

--- control-plane-ubuntu20.04.sh
+++ control-plane-ubuntu20.04.sh
@@ -1,2 +1,3 @@
 curl -s https://raw.githubusercontent.com/mikechesterwang/setup-kubeadm/main/setup-ubuntu20.04.sh | sudo bash
-sudo kubeadm init --cri-socket unix:///var/run/crio/crio.sock --pod-network-cidr 10.244.0.0/16 --control-plane-endpoint $(hostname):6443
+sudo kubeadm init --cri-socket unix:///var/run/crio/crio.sock --pod-network-cidr 10.244.0.0/16 --control-plane-endpoint $(hostname):
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
