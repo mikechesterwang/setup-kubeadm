c7c87b3244700b6fe9a317794330e84aecdc6bf5
Mike Chester Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-05-17 15:21:29 +0800

    Delete get_token.sh

--- get_token.sh
+++
@@ -1,9 +0,0 @@
-if ! [ -x "$(command -v jq)" ]; then
-    echo "jq not found, please install jq at first: https://stedolan.github.io/jq/download/" 
-    exit 1
-fi
-
-# parse input
-eval "$(jq -r '@sh "user=\(.user) ip=\(.ip) private_key_path=\(.private_key_path)"')"
-
-jq -n --arg token "$(ssh -o "StrictHostKeyChecking no" $user@$ip -i $private_key_path "sudo kubeadm token create")" --arg cert "$(ssh $user@$ip -i $private_key_path "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'")" '{"token": $token, "cert": $cert}' 
