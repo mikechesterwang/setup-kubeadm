b51ef846bab2ea26635da5c1c943a9ad4c7a64e4
Mike Chester Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-05-17 17:09:50 +0800

    Update setup-ubuntu20.04.sh

--- setup-ubuntu20.04.sh
+++ setup-ubuntu20.04.sh
@@ -16,6 +16,11 @@ sudo systemctl start crio.service
 sudo apt install -y cri-tools
 
 # iptables
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
